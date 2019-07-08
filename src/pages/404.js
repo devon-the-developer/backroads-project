d2c85f78e4edc62e89c06870dae13eea600bcd90
Devon Lumsden <devonlumsden@mail.com>
2019-07-08 19:41:02 +1200

    add banner and all css modules

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import {Link} from 'gatsby'
 import Layout from '../components/Layout'
+import styles from '../css/error.module.css'
+import Banner from '../components/Banner'
 
 export default function error() {
     return (
         <Layout>
-            hello from error page
+            <header className={styles.error}>
+                <Banner title="oops it's a dead end">
+                    <Link to='/' className='btn-white'>
+                        back to home page
+                    </Link>
+                </Banner>
+            </header>
         </Layout>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,16 @@
 import React from "react"
+import { Link } from 'gatsby'
 import Layout from '../components/Layout'
 import SimpleHero from '../components/SimpleHero'
-
+import Banner from '../components/Banner'
 export default () => (
     <Layout>
-        <SimpleHero />
+        <SimpleHero>
+            <Banner title='continue exploring' info='Some info about the website that should be Lorem. Eius, officius'>
+            <Link to='/tours' className='btn-white'>
+                Explore Tours
+            </Link>
+            </Banner>
+        </SimpleHero>
     </Layout>
 )
