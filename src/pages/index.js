343c48e1ee6a1400448cf765aef06646a0b40579
Devon Lumsden <devonlumsden@mail.com>
2019-07-09 15:24:24 +1200

    add services comp

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,16 +4,18 @@ import Layout from '../components/Layout'
 import SimpleHero from '../components/SimpleHero'
 import Banner from '../components/Banner'
 import About from '../components/Home/About.jsx'
+import Services from '../components/Home/Services.jsx'
 
 export default () => (
     <Layout>
         <SimpleHero>
             <Banner title='continue exploring' info='Some info about the website that should be Lorem. Eius, officius'>
-            <Link to='/tours' className='btn-white'>
-                Explore Tours
-            </Link>
+                <Link to='/tours' className='btn-white'>
+                    Explore Tours
+                </Link>
             </Banner>
         </SimpleHero>
         <About />
+        <Services />
     </Layout>
 )
